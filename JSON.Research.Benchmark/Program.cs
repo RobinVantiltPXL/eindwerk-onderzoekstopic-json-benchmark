8f5d49885115c5cbc5cecd52c4cdf0074e32f863
RobinVantiltPXL <robin.vantilt@student.pxl.be>
2023-05-14 23:02:18 +0200

    Fix delete execution of deleted benchmark

--- JSON.Research.Benchmark/Program.cs
+++ JSON.Research.Benchmark/Program.cs
@@ -2,5 +2,4 @@
 using JSON.Research.Benchmark.Benchmarks;
 
 BenchmarkRunner.Run<JSONDeserializeBenchmark>();
-BenchmarkRunner.Run<JSONDeserializeAdmObjectBenchmark>();
 BenchmarkRunner.Run<JSONSerializeBenchmark>();
